5c274d3c9a052ee284caa6c1d42ec0cf25736848
ParkalotDev3 <parkalotdev3@ParkalotDev3s-MacBook-Air.local>
2024-02-20 22:11:56 +0100

    docs: release @excalidraw/excalidraw@1.0.0  🎉

--- scripts/prerelease.js
+++ scripts/prerelease.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 const util = require("util");
-const path = require("path");
 const exec = util.promisify(require("child_process").exec);
 const updateChangelog = require("./updateChangelog");
 
